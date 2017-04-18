c6e681c679096da7a2dc639f97c9095725226b79
nurulnabi <noor.csarchitect@gmail.com>
2017-04-19 00:42:19 +0530

    function inside Layout

--- reactjs/js/client.js
+++ reactjs/js/client.js
@@ -2,16 +2,20 @@
 * @Author: nurulnabi
 * @Date:   2017-04-15 00:38:21
 * @Last Modified by:   nurulnabi
-* @Last Modified time: 2017-04-15 00:42:56
+* @Last Modified time: 2017-04-19 00:40:38
 */
 
 import React from "react";
 import ReactDOM from "react-dom";
 
 class Layout extends React.Component{
+	getVal(){
+		return "noor";
+	}
+
 	render(){
 		return(
-			<h1>It's Working!</h1>
+			<h1>It's { this.getVal()}</h1>
 		);
 	}
 }
