052d37d3edac6827c7422556a94808d5b9596e63
nurulnabi <noor.csarchitect@gmail.com>
2017-04-19 23:30:22 +0530

    js events and data change

--- reactjs/js/components/Layout.js
+++ reactjs/js/components/Layout.js
@@ -2,7 +2,7 @@
 * @Author: nurulnabi
 * @Date:   2017-04-19 00:45:16
 * @Last Modified by:   nurulnabi
-* @Last Modified time: 2017-04-19 02:13:28
+* @Last Modified time: 2017-04-19 23:25:38
 */
 
 import React from "react";
@@ -14,16 +14,15 @@ export default class Layout extends React.Component{
 		super();
 		this.state = { name: "Good!"}
 	}
+	
+	changeTitle(title){
+		this.setState({name:title});
+	}
 
 	render(){
-		const msg = "Noor welcomes you!"
-		setTimeout(function(that){
-			that.setState({ name: "Nope!"})
-		},1000,this)
 		return(
 			<div>
-				{ this.state.name }
-				<Header title={msg}/>
+				<Header  changeTitle={this.changeTitle.bind(this)} title={this.state.name}/>
 				<Footer />
 			</div>
 		);
--- reactjs/js/components/header.js
+++ reactjs/js/components/header.js
@@ -2,16 +2,23 @@
 * @Author: nurulnabi
 * @Date:   2017-04-19 00:45:16
 * @Last Modified by:   nurulnabi
-* @Last Modified time: 2017-04-19 02:09:17
+* @Last Modified time: 2017-04-19 23:28:45
 */
 
 import React from "react";
 import Title from "./header/title"
 
 export default class Header extends React.Component{
+	handleChange(e){
+		const title = e.target.value;
+		this.props.changeTitle(title);
+	}
 	render(){
 		return(
-			<Title title={this.props.title}/>
+			<div>
+				<Title title={this.props.title}/>
+				<input value={this.props.title} onChange={this.handleChange.bind(this)} />
+			</div>
 		);
 	}
 }
